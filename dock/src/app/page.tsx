288f5dbb8749b44d372777cebe5d9d3eb252d215
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-10-07 18:36:27 +0300

    init-next-app

---
+++ dock/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+
+        <h1 className="w-full font-semibold text-1xl text-center">
+          Poseidon's DOCK
+        </h1>
+
+    </main>
+  )
+}

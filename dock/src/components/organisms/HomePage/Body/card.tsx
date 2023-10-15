2327c2a5d03209b0b036da173debfb3f2b44c4a0
Roman Njoroge <roman.njoroge@njuguna.com>
2023-10-15 14:11:44 +0300

    feat: Made cards based on some prop

--- dock/src/components/organisms/HomePage/Body/card.tsx
+++ dock/src/components/organisms/HomePage/Body/card.tsx
@@ -1,19 +1,28 @@
 "use client"
+import { CardItemProps } from "./cardItem";
+
+export interface CardProps {
+  items: CardItemProps[]
+}
 
 import CardItem from "./cardItem";
 
- function Card() {
+ function Card(props: CardProps) {
     return ( 
       <div className="flex flex-row justify-center">
         <div className="flex flex-col w-4/5 items-center justify-center p-10">
           <div className="grid grid-cols-3 gap-x-10 gap-y-10 w-full">
             {
-              [...Array(20).fill(0)]?.map((k, i)=>{
-                return (
-                  <CardItem
-                    key={i}
-                  />
-                )
+              // props.map((k, i)=>{
+              //   return (
+              //     <CardItem
+              //       key={i}
+              //       {...k}
+              //     />
+              //   )
+              // })
+              props.items.map((item, i) => {
+                return <CardItem name={item.name} description={item.description} key={i}/>
               })
             }
           </div>
--- dock/src/components/organisms/HomePage/Body/cardItem.tsx
+++ dock/src/components/organisms/HomePage/Body/cardItem.tsx
@@ -3,18 +3,21 @@ import Link from 'next/link'
 import React from "react";
 import { Button } from "../../../ui/button";
 
-function CardItem() {
+export interface CardItemProps {
+  name: string
+  description: string
+}
+
+function CardItem(props: CardItemProps) {
   return (
     <div className="flex flex-col items-center justify-start rounded-md overflow-hidden shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
       <div className="flex flex-row items-center justify-center w-full  bg-slate-300 px-5 py-5">
-        SpongeBob
+        {props.name}
       </div>
       <div className="flex flex-col items-start justift-start p-3 w-full space-y-2">
-        <span>Discuss all things SpongeBob</span>
+        <span>{props.description}</span>
 
-        <Link href='/Posts' legacyBehavior= {true}>
         <Button size="sm" className="self-center my-5">Request to Join</Button>
-        </Link>
       </div>
     </div>
   );
